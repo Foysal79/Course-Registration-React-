1f13eb40654ba75f1ceed8922704a17b8452ce53
Foysal79 <onthor312001@gmail.com>
2023-09-15 13:44:23 +0600

    fully structured website and cart section dinamic data load .

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,23 +1,26 @@
 
 
-const Cart = () => {
+const Cart = ({selectedData, totalCredit, remainingCredit, totalPrice}) => {
+    
+    
     return (
-        <div className="p-4">
+        <div className="p-3">
              <h3 className="border-b-2 p-4 text-[#2F80ED] text-lg font-bold">
-             Credit Hour Remaining 7 hr
+             Credit Hour Remaining {remainingCredit} hr
              </h3>
              <div className="border-b-2 pb-4">
              <h1 className="mt-2 mb-4 text-xl font-bold">
                 Course Name 
              </h1>
+             
              <ul className="text-base font-normal">
-             <li>
-                1 .Introduction to c programming
-             </li>
+             {
+                selectedData.map((course, index) =>  <li key={course.id}> {index + 1} . {course.course_name}</li>)
+             }
              </ul>
              </div>
-             <p className="border-b-2 pt-3 pb-3 text-base font-semibold">Total Credit Hour : 13</p>
-             <p className="mt-4 font-bold text-base">Total Price : 48000 USD</p>
+             <p className="border-b-2 pt-3 pb-3 text-base font-semibold">Total Credit Hour : {totalCredit}</p>
+             <p className="mt-4 font-bold text-base">Total Price : {totalPrice} USD</p>
         </div>
     );
 };
